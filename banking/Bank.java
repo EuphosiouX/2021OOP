daf0f33e4f03385a1f23058bcb09e0ac4e51304e
Michael C <71363695+EuphosiouX@users.noreply.github.com>
2021-04-22 22:12:47 +0700

    Update Bank.java

--- banking/Bank.java
+++ banking/Bank.java
@@ -1,6 +1,5 @@
 package com.application.banking;
 
-import java.util.Arrays;
 import java.util.ArrayList;
 
 public class Bank {
@@ -8,20 +7,50 @@ public class Bank {
     private int numberOfCustomers = 0;
     private String bankName;
 
-    public Bank(String bName){
+    public Bank(String bName) {
         this.bankName = bName;
     }
 
-    public void addCustomer(String f, String l){
-        this.customer.add(new Customer(f, l));
+    public void addCustomer(Customer customer) {
+        this.customer.add(new Customer(customer.getFirstName(), customer.getLastName(), customer.getAccount()));
         numberOfCustomers += 1;
     }
 
-    public int getNumberOfCustomers(){
+    public int getNumberOfCustomers() {
         return numberOfCustomers;
     }
 
     public Customer getCustomer(int index) {
         return this.customer.get(index);
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void printAll() {
+        if (customer.isEmpty()) {
+            System.out.println("No Data Found...");
+        }
+        else {
+            for (int i = 0; i < customer.size(); i++) {
+                System.out.println("Account: " + i);
+                System.out.println("First Name: " + getCustomer(i).getFirstName());
+                System.out.println("Last Name: " + getCustomer(i).getLastName());
+                System.out.println("Balance: $" + getCustomer(i).getAccount().getBalance());
+            }
+        }
+    }
+
+    public void print(int i) {
+        if (customer.isEmpty()) {
+            System.out.println("No Data Found...");
+        }
+        else {
+            System.out.println("Account: " + i);
+            System.out.println("First Name: " + getCustomer(i).getFirstName());
+            System.out.println("Last Name: " + getCustomer(i).getLastName());
+            System.out.println("Balance: $" + getCustomer(i).getAccount().getBalance());
+        }
     }
 }
